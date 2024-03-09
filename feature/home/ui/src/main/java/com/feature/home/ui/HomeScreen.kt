2fe0c03adebc39af7bb0e994a8c27947012428a1
Camilo Cruz <c.cruz@globant.com>
2024-03-09 12:38:30 -0500

    Refactor CreateEntry
    - Add CreateEntryViewModel

--- feature/home/ui/src/main/java/com/feature/home/ui/HomeScreen.kt
+++ feature/home/ui/src/main/java/com/feature/home/ui/HomeScreen.kt
@@ -1,5 +1,3 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.feature.home.ui
 
 import android.util.Log
@@ -13,7 +11,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold

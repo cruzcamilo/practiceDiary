8f6ac737463dd6df4067c245b73fd41d250f79a9
Camilo Cruz <c.cruz@globant.com>
2024-03-10 11:02:09 -0500

    Use empty screen message string

--- feature/home/ui/src/main/java/com/feature/home/ui/HomeScreen.kt
+++ feature/home/ui/src/main/java/com/feature/home/ui/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.feature.home.ui
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,6 +20,7 @@ import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
@@ -41,18 +41,24 @@ fun HomeScreen(
         initialValue = EntriesUiState.Loading,
         key1 = lifecycle,
         key2 = homeViewModel
-    ){
-        lifecycle.repeatOnLifecycle(state = Lifecycle.State.STARTED){
-            homeViewModel.uiState.collect{ value = it}
+    ) {
+        lifecycle.repeatOnLifecycle(state = Lifecycle.State.STARTED) {
+            homeViewModel.uiState.collect { value = it }
         }
     }
 
     Scaffold(
         floatingActionButton = { FabButton { homeViewModel.onFabPressed() } }
-        ) { padding ->
-        when(uiState) {
-            is EntriesUiState.Error -> { TODO() }
-            EntriesUiState.Loading -> { CircularProgressIndicator() }
+    ) { padding ->
+        when (uiState) {
+            is EntriesUiState.Error -> {
+                TODO()
+            }
+
+            EntriesUiState.Loading -> {
+                CircularProgressIndicator()
+            }
+
             is EntriesUiState.Success -> {
                 val entries = (uiState as EntriesUiState.Success).diaryEntries
                 if (entries.isEmpty()) {
@@ -84,7 +90,7 @@ private fun NoEntriesScreen(padding: PaddingValues) {
         contentAlignment = Alignment.Center
     ) {
         Text(
-            text = "Add a new entry to start tracking your practice",
+            text = stringResource(R.string.empty_screen),
             fontSize = 14.sp
         )
     }
